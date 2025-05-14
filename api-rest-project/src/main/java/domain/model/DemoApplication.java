74eb19d8e755ad9fc7f4961cd0a657f8069f0ed8
Ismael Mendonca <mendoncasilva545@gmail.com>
2025-05-14 16:30:48 -0300

    Atualização do projeto api-rest

--- api-rest-project/src/main/java/domain/model/DemoApplication.java
+++ api-rest-project/src/main/java/domain/model/DemoApplication.java
@@ -1,4 +1,4 @@
-package projeto_api_rest_spring_boot3.demo;
+package domain.model;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
